875afda98df6415b8e475e09f82d998e19850bcb
Steven John <stevenjpx2@gmail.com>
2026-03-15 18:21:19 +0530

    feat: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,5 +10,22 @@ export default defineNuxtConfig({
   ],
   devtools: { enabled: true },
   compatibilityDate: "2024-04-03",
+
+  app: {
+    head: {
+      title: "Confident Christian Conversations",
+      meta: [
+        {
+          name: "description",
+          content:
+            "A free 6-month apologetics training programme in partnership with Reasonable Faith and Ratio Christi. Equipping Indian Christians to defend their faith in campuses, workplaces, and communities.",
+        },
+      ],
+      link: [
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      ],
+    },
+  },
 });
 
